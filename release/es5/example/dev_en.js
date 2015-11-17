4fec2c804a0da21483ca908c2b11fa2987d3ff3e
Sun Haohao <viringbells@qq.com>
2015-11-17 18:21:05 +0800

    add release

---
+++ release/es5/example/dev_en.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var _ = require('..');
+
+var _2 = _interopRequireDefault(_);
+
+var _debug2 = require('debug');
+
+var _debug3 = _interopRequireDefault(_debug2);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/**
+ * Example of lark-config
+ **/
+
+var debug = (0, _debug3.default)('lark-config');
+
+debug('Example: set app.js as the main module');
+process.mainModule = module;
+
+debug('Example: loading configs');
+var configs = (0, _2.default)('configs', {
+  env: 'development',
+  locale: 'en'
+});
+
+debug('Example: print configs');
+console.log(configs);
